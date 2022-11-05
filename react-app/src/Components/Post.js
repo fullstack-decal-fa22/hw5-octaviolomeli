8547ed86362248ae6f0fb213d7ca5b099aae7a24
Octavio L <62814754+octaviolomeli@users.noreply.github.com>
2022-11-04 22:59:06 -0700

    Add files via upload

--- react-app/src/Components/Post.js
+++ react-app/src/Components/Post.js
@@ -1,5 +1,4 @@
-// ___________________ import statement
-
+import Comments from "./Comments"
 const Post = ({ title, body, comments, id }) => {
   console.log(id)
   return <div style={{ textAlign: 'left', marginBottom: '12px'}}>
@@ -7,7 +6,7 @@ const Post = ({ title, body, comments, id }) => {
       {title}
     </h3>
     <p>{body}</p>
-    <Comments _________ _________ />
+    <Comments postId={id} comments={comments} />
   </div>
 }
 
